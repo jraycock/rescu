27fa38fb58070994aa046ce571a69f61bee5879b
Andrew Thorp <andrewpthorp@gmail.com>
2012-03-13 14:45:51 -0400

    scores

--- js/rescu.js
+++ js/rescu.js
@@ -1,4 +1,26 @@
 var RESCU = RESCU || {};
 
-// Default Score
-RESCU.score = 0;
+// Default Scores
+RESCU.scores = RESCU.scores || {};
+
+RESCU.scores = {
+  total: 0,
+  foundation: 0,
+  walls: 0,
+  windows: 0,
+  roof: 0,
+  chimney: 0,
+  vents: 0,
+  gutters: 0,
+
+  calculateTotal: function(){
+    this.total = this.walls + this.foundation + this.windows + this.roof + this.chimney + this.vents + this.gutters;
+
+    var $scoreEl = $("#score h3");
+    if ($scoreEl){
+      $scoreEl.html(this.total);
+    }
+  }
+};
+
+RESCU.currPage = "foundation";
