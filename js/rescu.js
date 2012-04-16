956fc11ee5913d32715da12bf41c4385abdfe523
Andrew Thorp <andrewpthorp@gmail.com>
2012-04-16 10:26:31 -0400

    working on JSON driven questions

--- js/rescu.js
+++ js/rescu.js
@@ -1,5 +1,34 @@
 var RESCU = RESCU || {};
 
+// Default Questions
+RESCU.questions = RESCU.questions || {};
+
+RESCU.filterQuestions = function(view){
+  return _.filter(RESCU.questions, function(question){
+    return question.view === view;
+  });
+}
+
+RESCU.displayQuestions = function(){
+  _.each(questions, function(question){
+    var $section = $("<section></section>"),
+        $fieldset = $("<fieldset></fieldset>");
+
+    $section.append($("<h2>" + question.question + "</h2>"));
+    _.each(question.answers, function(answer){
+      $fieldset.append("<input id='"+answer.id+"' name='"+answer.name+"' value='"+answer.points+"' type='"+answer.type+"' />");
+      $fieldset.append("<label for='"+answer.id+"'>"+answer.answer+"</label><br />");
+      $section.append($fieldset);
+    });
+
+    if (question.moreInfo !== undefined){
+      $section.append("<a href='"+question.moreInfo.link+"' target='_blank'>"+question.moreInfo.text+"</a>")
+    }
+
+    $(".questions").append($section);
+  });
+}
+
 // Default Scores
 RESCU.scores = RESCU.scores || {};
 
